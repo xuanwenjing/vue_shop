d3cbae8bee57fb45dd54666ef662cb2093c6055e
121341107@qq.com <121341107@qq.com>
2020-10-20 13:55:53 +0800

    complete user function

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../components/Login.vue';
+import Home from '../components/Home.vue';
+import Welcome from '../components/WelCome.vue';
+import User from '../components/user/User.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
+        },
+        { path: '/users', component: User }
+      ]
+    }
   ]
-})
+});
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
   // to 将要访问的路径
   // from 代表从哪个路径来
   // next 放行  next()    next('/login')强制跳转
-  if (to.path === '/login') return next()
+  if (to.path === '/login') return next();
   // 获取token
-  const tokenStr = window.sessionStorage.getItem('token')
-  if (!tokenStr) return next('/login')
-  next()
-})
-export default router
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login');
+  next();
+});
+export default router;
